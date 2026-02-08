c70bfa24f8d4eceb9b1a03a56bda3062abe21243
Santiago Osorio Parra <santiago.osorio@jala.university>
2026-02-08 12:26:06 +0000

    Lb 2/signal comunication chat gameplay

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TankContainerComponent } from './features/tank/tank-container.component';
+import { WaitingRoomComponent } from './features/waiting-room/waiting-room.component';
 
 export const routes: Routes = [
-  { path: '', component: TankContainerComponent }
+  { path: '', component: WaitingRoomComponent },
+  { path: 'tank', component: TankContainerComponent }
 ];
